0717961b3d5703f43b83cf834531129bead37939
PhanVanLinh <phanvanlinh.94vn@gmail.com>
2018-06-17 11:01:52 +0700

    [Java] Add bottle sample

---
+++ java/src/Part1/bottle/Bottle.java
@@ -0,0 +1,33 @@
+package Part1.bottle;
+
+public class Bottle {
+
+    private static String song() {
+        return compose(99, 0);
+    }
+
+    private static String compose(int hi, int lo) {
+        StringBuilder poem = new StringBuilder();
+        for (int n = hi; n >= lo; n--) {
+            poem.append(compose(n));
+            poem.append("\n");
+        }
+        return poem.toString();
+    }
+
+    private static String compose(int n) {
+        String verse = "";
+        verse += n == 0 ? "No more bottles" : (n == 1) ? "1 bottle" : n + " bottles";
+        verse += " of beer on the wall, ";
+        verse += n == 0 ? "No more bottles" : (n == 1) ? "1 bottle" : n + " bottles" + " of beer.";
+        verse += "\n";
+        verse += n == 1 ? "Go to the store and buy some more, " : "Take one down and pass it around, ";
+        verse += n == 1 ? "No more bottles" : (n == 2) ? "1 bottle" : n == 0 ? "99 bottles" : (n - 1) + "bottles";
+        verse += " of beer on the wall.";
+        return verse;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(song());
+    }
+}
